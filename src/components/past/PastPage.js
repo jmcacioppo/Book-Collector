9b6dc09caf2a5e07b27b2ed319e89375c46c1aa0
jmcacioppo <jmcacioppo0531@gmail.com>
2018-12-25 17:08:57 -0500

    Working on past page

--- src/components/past/PastPage.js
+++ src/components/past/PastPage.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import BookList from './elements/BookListComponent';
 
 class Past extends React.Component {
   render() {
     return (
       <section className="TextCenter">
         <h3 className="SectionTitle">Past Books Read</h3>
+
+        <Button active>2018</Button>
+        <Button>2019</Button>
+        <Button>2020</Button>
+
+        <br />
+        <br />
+
+        <BookList></BookList>
       </section>
     )
   }
---
+++ src/components/past/elements/BookListComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BookList = ({ books }) => {
+
+  return (
+    <div>
+      Book List
+    </div>
+  )
+}
+
+export default BookList;
